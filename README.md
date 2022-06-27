f3df86c27af1aea8f1a5e314fac4d81d4571c422
deb boro <debajit.boro666@gmail.com>
2022-06-26 20:50:48 -0400

    badge updated

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
-# ewrwerwer
+# werwerwer
 
 
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-      
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+   
 
 ## Description
-werwerwer
+werwerwerwerwerwer
 
 ## Table of Contents 
 - [Installation](#Installation)
@@ -17,13 +17,13 @@ werwerwer
 - [Questions](#Questions)
 
 ## Installation
-werwerwer
+werwerwerwerwer
 
 ## Usage
-werwerwer
+werwerwerwerwerwerwerwerwerwr
 
 ## Contribution
-werwerwer
+werwerwerwerwerwer
 
 ## License
 
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,23 +1,23 @@
 const generateLicenseBadge = (license) => {
   if (license === 'Apache License 2.0') {
     return `
-    [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
    `
   } else if (license === 'MIT License') {
     return `
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
       `
   } else if (license === 'Boost Software License 1.0') {
     return `
-    [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
       `
   } else if (license === 'Eclipse Public License 1.0') {
     return `
-      [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
       `
   } else if (license === 'Mozilla Public License 2.0') {
     return `
-    [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
       `
   } else if (license === null) {
   }
